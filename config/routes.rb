3dd25c07850e05d34b91caca867672dd7f608531
sousou1216 <sousou1216@yahoo.co.jp>
2023-10-21 10:01:26 +0900

    bootstrapに関してcssまで導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'items/index'
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  get "items", to: "items#index"
+  root to: "items#index"
 end
