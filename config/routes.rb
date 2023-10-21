33dc69a77250fbd9b64d0cb4d6a69ec0ac8a8c50
sousou1216 <sousou1216@yahoo.co.jp>
2023-10-21 10:58:46 +0900

    taskのMVCを削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,5 @@
 
 Rails.application.routes.draw do
   get 'items/index'
-  resources :tasks
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  get "items", to: "items#index"
   root to: "items#index"
 end
