c93976281af2fee447ddc274260e93c4d2abd45b
sousou1216 <sousou1216@yahoo.co.jp>
2023-10-22 11:43:56 +0900

    seedを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 if Rails.env.development?
-  (1..50).each do |i|
-    Item.create(item_name: "商品#{i}", item_price: "金額#{i}")
+  (1..5).each do |i|
+    Item.create(name: "商品#{i}", price: i)
   end
 end
