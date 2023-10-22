5e9bd27079b40d8db49aebd1c8a2a8b71b3e90fe
sousou1216 <sousou1216@yahoo.co.jp>
2023-10-22 23:31:31 +0900

    heroku対応のためseedの設定を変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-if Rails.env.development?
-  (1..5).each do |i|
-    Item.create(name: "商品#{i}", price: i)
-  end
+
+# if Rails.env.development?
+#   (1..5).each do |i|
+#     Item.create(name: "商品#{i}", price: i)
+#   end
+# end
+
+(1..5).each do |i|
+  Item.create(name: "商品#{i}", price: i)
 end
