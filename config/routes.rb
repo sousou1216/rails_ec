aa632f69542589b70e36e694882309b9721c039a
sousou1216 <sousou1216@yahoo.co.jp>
2023-10-22 21:39:08 +0900

    herokuのエラー対応のためコメントアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'items', to: 'items#index'
-  get 'items/new', to: 'items#new'
-  post 'items', to: 'items#create'
+  # get 'items', to: 'items#index'
+  # get 'items/new', to: 'items#new'
+  # post 'items', to: 'items#create'
 
   root to: 'items#index'
 end
