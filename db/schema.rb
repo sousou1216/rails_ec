157d2ce8638e451a39e93072d4da2a9db1e7773b
sousou1216 <sousou1216@yahoo.co.jp>
2023-10-22 18:15:55 +0900

    herokuのエラー対応のための修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_22_015845) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_22_013940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,14 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_22_015845) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.integer "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
