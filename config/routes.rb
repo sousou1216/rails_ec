05a7ae8404032639701bb914597045a0bd3c1db2
sousou1216 <sousou1216@yahoo.co.jp>
2023-10-28 15:19:58 +0900

    basic認証対応など

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+  end
+  
   resources :items, only: %i[index new create show]
   root 'items#index'
 end
