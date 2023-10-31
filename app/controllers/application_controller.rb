fdbaaa7a1165679439c018553d1867cdeac4b580
sousou1216 <sousou1216@yahoo.co.jp>
2023-11-01 08:03:52 +0900

    heroku対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     redirect_to login_url unless current_user
   end
 
-  findメソッドではnilの時エラーとなるためfind_byを使う
+  # findメソッドではnilの時エラーとなるためfind_byを使う
   def current_user
     @current_user || = User.find_by(id: session[:user_id] if session[:user_id])
   end
