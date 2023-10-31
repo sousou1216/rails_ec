d33cb3872aace98cb8636da5c7186db953f4e17c
sousou1216 <sousou1216@yahoo.co.jp>
2023-11-01 08:02:26 +0900

    heroku対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # before_action :login_required
+  before_action :login_required
   # 全てのビューから使えるようにする
-  # helper_method :current_user
+  helper_method :current_user
 
   # アクションではないメソッドはprivateで定義する
-  # private
+  private
 
-  # def login_required
-  #   redirect_to login_url unless current_user
-  # end
+  def login_required
+    redirect_to login_url unless current_user
+  end
 
-  # findメソッドではnilの時エラーとなるためfind_byを使う
-  # def current_user
-  #   @current_user || = User.find_by(id: session[:user_id] if session[:user_id])
-  # end
+  findメソッドではnilの時エラーとなるためfind_byを使う
+  def current_user
+    @current_user || = User.find_by(id: session[:user_id] if session[:user_id])
+  end
 end
