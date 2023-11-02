f211842a3f3ad32ebbb0650dc871872934f4fed0
sousou1216 <sousou1216@yahoo.co.jp>
2023-11-02 13:17:42 +0900

    コード整形

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,11 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
-
-  def get_session
-    session_id = session[:session_id]
-    redirect_to root_url
-  end
+  # セッションIDはデフォルトで生成される
+  # session[:session_id]
 
   def destroy
     reset_session
     redirect_to root_url
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
   namespace :admin do
     resources :items, only: %i[index new create edit update destroy]
     root 'items#index'
