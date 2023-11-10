bd9fe2f7f7f6c86640d7902f44fbe36a39128593
sousou1216 <sousou1216@yahoo.co.jp>
2023-11-10 14:15:19 +0900

    セッション管理方法の変更によりカートへの追加と削除を修正した

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,21 +1,16 @@
-# frozen_string_literal: true
-
 class CartsController < ApplicationController
   def index; end
 
   # カートにアイテムを追加
   def add_to_cart
     item = Item.find(params[:id])
-    # binding.pry
-    # 初期化しないとエラーになる
-    session[:cart] ||= []
     if params[:quantity]
-      item_quantity = params[:quantity]
-      item_quantity.to_i.times do
-        session[:cart] << item
+      item_quantity = params[:quantity].to_i
+      item_quantity.times do
+        current_cart.items << item
       end
     else
-      session[:cart] << item
+      current_cart.items << item
     end
 
     # カートにアイテムを追加した後の処理
@@ -24,17 +19,13 @@ class CartsController < ApplicationController
 
   # カート内のアイテムを削除
   def delete_from_cart
-    item = params[:id]
+    delete_id = params[:id].to_i
 
-    # session[:cart] から最初に特定の条件に一致する要素を削除
-    item_delete = session[:cart].find { |i| i['id'] == item['id'].to_i }
-    if item_delete
-      index_delete = session[:cart].find_index(item_delete)
-      session[:cart].delete_at(index_delete)
-    end
-
-    # session[:cart] から特定の条件に一致する要素を削除
-    # session[:cart].delete_if { |i| i['id'] == item['id'].to_i }
+    # ※関連付けを解除しないとitemsテーブルから削除されたりエラーとなったりする
+    # 指定された item_id を持つカート内の最初の cart_item_relation を取得
+    delete_relation = current_cart.cart_item_relations.find_by(item_id: delete_id)
+    # cart_item_relation が存在する場合は、その関連を削除
+    delete_relation.destroy if delete_relation.present?
 
     # カートからアイテムを削除した後の処理
     redirect_to carts_path, notice: 'カートから削除しました。'
