e1f92728b21557f712570e7c6a64d0b9328e068a
sousou1216 <sousou1216@yahoo.co.jp>
2023-11-10 14:14:22 +0900

    セッションにcart_idを設定した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
+  helper_method :current_cart
+
+  private
+
+  def current_cart
+    # セッションにcart_idがあればそれを使用し、なければ新しいカートを作成
+    @current_cart ||= Cart.find_by(id: session[:cart_id]) || Cart.create
+    # カートが新しく作成された場合はセッションにcart_idを保存
+    session[:cart_id] ||= @current_cart.id
+
+    @current_cart
+  end
 end
