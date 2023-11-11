948964b036a80013bfe22f6ed8f85f3cc4300eca
sousou1216 <sousou1216@yahoo.co.jp>
2023-11-11 09:37:50 +0900

    カード追加のロジックをCartモデルへ

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,14 +6,8 @@ class CartsController < ApplicationController
   # カートにアイテムを追加
   def add_to_cart
     item = Item.find(params[:id])
-    if params[:quantity]
-      item_quantity = params[:quantity].to_i
-      item_quantity.times do
-        current_cart.items << item
-      end
-    else
-      current_cart.items << item
-    end
+    # params[:quantity]がnilの場合に1を返す
+    current_cart.add(item, params[:quantity].presence || 1)
 
     # カートにアイテムを追加した後の処理
     redirect_to items_path, notice: 'カートに追加しました。'
@@ -24,8 +18,8 @@ class CartsController < ApplicationController
     delete_id = params[:id].to_i
 
     # ※関連付けを解除しないとitemsテーブルから削除されたりエラーとなったりする
-    # 指定された item_id を持つカート内の最初の cart_item_relation を取得
-    delete_relation = current_cart.cart_item_relations.find_by(item_id: delete_id)
+    # 指定された item_id を持つカート内の最初の cart_items を取得
+    delete_relation = current_cart.cart_items.find_by(item_id: delete_id)
     # cart_item_relation が存在する場合は、その関連を削除
     delete_relation.destroy if delete_relation.present?
 
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class Cart < ApplicationRecord
-  has_many :cart_item_relations, dependent: :delete_all
-  has_many :items, through: :cart_item_relations
+  has_many :cart_items, dependent: :delete_all
+  has_many :items, through: :cart_items
+
+  def add(item, quantity)
+    item_quantity = quantity.to_i
+    item_quantity.times { items << item }
+  end
 end
