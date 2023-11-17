70e8ab312424498f4ca3ae2795212d11cf0466ea
sousou1216 <sousou1216@yahoo.co.jp>
2023-11-17 21:47:22 +0900

    メール関連の設定

---
+++ app/mailers/purchase_mailer.rb
@@ -0,0 +1,11 @@
+class PurchaseMailer < ApplicationMailer
+  def creation_email(bill, items)
+    @bill = bill
+    @items = items
+    mail(
+      subject: '購入完了メール',
+      to: @bill.email,
+      from: 'purchase@example.com'
+    )
+  end
+end
