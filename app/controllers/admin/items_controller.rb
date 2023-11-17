f406a1a26b407baf5aae2ca8fe25e59c9570b87f
sousou1216 <sousou1216@yahoo.co.jp>
2023-11-17 22:07:31 +0900

    不要なコード削除

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module Admin
   class ItemsController < ApplicationController
     before_action :basic_auth
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class CartsController < ApplicationController
   def index; end
 
