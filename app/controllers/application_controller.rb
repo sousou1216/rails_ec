1596dd07aa11941c7e608d931abe50d90c9faeb7
sousou1216 <sousou1216@yahoo.co.jp>
2023-11-23 21:33:44 +0900

    一意性制約違反に対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,24 @@ class ApplicationController < ActionController::Base
   private
 
   def current_cart
+    # 以下のやり方だと一意性制約違反となりエラーとなる場合がある
     # セッションにcart_idがあればそれを使用し、なければ新しいカートを作成
-    @current_cart = Cart.find_or_create_by(id: session[:cart_id])
+    # @current_cart = Cart.find_or_create_by(id: session[:cart_id])
     # カートが新しく作成された場合はセッションにcart_idを保存
-    session[:cart_id] ||= @current_cart.id
+    # session[:cart_id] ||= @current_cart.id
+    # @current_cart
+
+    # 一意性制約違反に対応
+    if session[:cart_id]
+      # セッションに保存されたカートIDを使用してカートを検索
+      @current_cart = Cart.find_by(id: session[:cart_id])
+    end
+
+    unless @current_cart
+      # カートが見つからない場合は新しいカートを作成し、セッションに保存
+      @current_cart = Cart.create
+      session[:cart_id] = @current_cart.id
+    end
 
     @current_cart
   end
